eae52a691b79b4a4b886837b6d0af056d3316c0a
Hamza <hamzavherkaoui806@gmail.com>
2024-02-15 00:15:33 +0100

    modified

--- Small-Project-React/src/CihForm.js
+++ Small-Project-React/src/CihForm.js
@@ -1,35 +1,95 @@
 import "./CihStyles.css";
 import Modal from "./Modal";
+import { useState } from "react";
 
-export default function LoanForm() {
+export default function CihForm() {
+  const [showModal, setShowModal] = useState(false);
+  const [cihInput, setCihInput] = useState({
+    name: "",
+    phoneNumber: "",
+    age: "",
+    isOk: false,
+    salary: "",
+  });
+  let btnIsDisable =
+    cihInput.name == "" || cihInput.age == "" || cihInput.phoneNumber == "";
+
+  function handlBtn(event) {
+    event.preventDefault();
+    setShowModal(true);
+  }
+
+  function handlDivClick() {
+    if (showModal) {
+      setShowModal(false);
+    }
+  }
+
   return (
-    <div className="flex" style={{ flexDirection: "column" }}>
+    <div
+      onClick={handlDivClick}
+      className="flex"
+      style={{ flexDirection: "column" }}
+    >
       <form id="cih-form" className="flex" style={{ flexDirection: "column" }}>
         <h1 style={{ fontSize: "40px" }}>Requesting a CIH</h1>
         <hr></hr>
 
         <label>FULL NAME :</label>
-        <input />
+        <input
+          value={cihInput.name}
+          onChange={(event) => {
+            setCihInput({ ...cihInput, name: event.target.value });
+          }}
+        />
 
         <label>PHONE NUMBER :</label>
-        <input />
+        <input
+          value={cihInput.phoneNumber}
+          onChange={(event) => {
+            setCihInput({ ...cihInput, phoneNumber: event.target.value });
+          }}
+        />
 
         <label>AGE :</label>
-        <input />
+        <input
+          value={cihInput.age}
+          onChange={(event) => {
+            setCihInput({ ...cihInput, age: event.target.value });
+          }}
+        />
 
         <label style={{ marginTop: "20px" }}>Are you a employee: </label>
-        <input type="chckbox" />
+        <input
+          type="checkbox"
+          checked={cihInput.isOk}
+          onChange={(e) => {
+            setCihInput({ ...cihInput, isOk: e.target.checked });
+          }}
+        />
 
         <label>Salary</label>
-        <select>
+        <select
+          value={cihInput.salary}
+          onChange={(event) => {
+            setCihInput({ ...cihInput, salary: event.target.value });
+          }}
+        >
           <option>less then 500$</option>
           <option>500$</option>
           <option>height then 500$</option>
         </select>
 
-        <button id="cih-btn">Submit</button>
+        <button
+          className={btnIsDisable ? "disable" : ""}
+          onClick={handlBtn}
+          disabled={btnIsDisable}
+          id="cih-btn"
+        >
+          Submit
+        </button>
       </form>
-      {/* <Modal /> */}
+      <Modal isVisible={showModal} />
     </div>
   );
 }
--- Small-Project-React/src/Modal.jsx
+++ Small-Project-React/src/Modal.jsx
@@ -1,11 +1,13 @@
 import "./CihStyles.css";
 
-export default function Modal() {
-  return (
-    <div id="modal">
-      <div id="modal-content">
-        <h1>Login Successfully</h1>
+export default function Modal({isVisible}) {
+  if (isVisible) {
+    return (
+      <div id="modal">
+        <div id="modal-content">
+          <h1>Login Successfully</h1>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else (<></>)
 }
